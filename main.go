99108c0707c2b8284245b7bbe0a3621f4c8ce8a8
mhandyalf-raya <muhammad.furqon@bankraya.co.id>
2025-11-18 14:00:06 +0700

    Add Prometheus metrics endpoint

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"net/http"
+
 	_ "github.com/joho/godotenv/autoload"
 	routers "github.com/mhandyalf/go-passmanager/routers"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 	routers := routers.SetupRouter()
+	http.Handle("/metrics", promhttp.Handler())
 	routers.Run(":8080")
 }
